3098b5c7f10d0aa6bf63c3a8db925a126863ac97
FrizenVladyslav <FrizenVladyslav>
2020-03-14 23:30:23 +0200

    Setup environment && MongoDB connection

--- api/loaders/dependencyInjector.js
+++ api/loaders/dependencyInjector.js
@@ -4,10 +4,19 @@ import fs from 'fs';
 
 export default new Promise((resolve, reject) => {
   try {
+    const modelsDir = path.resolve(__dirname, '../models');
     const servicesDir = path.resolve(__dirname, '../services');
+    const models = fs.readdirSync(modelsDir);
     const services = fs.readdirSync(servicesDir);
     const replaceName = string => string.replace('.js', '');
 
+    // DI mongoose models
+    models.forEach(model => {
+      Container.set(
+        `${replaceName(model)}Model`,
+        require(`../models/${model}`).default,
+      );
+    });
     // DI services
     services.forEach(service => {
       Container.set(
