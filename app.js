e62d596ca2829853cfb465471bcdfeddc41727d7
FrizenVladyslav <FrizenVladyslav>
2020-03-15 15:15:54 +0200

    Api doc

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import compression from 'compression';
+import swaggerUi from 'swagger-ui-express';
 import config from './api/config';
 
 const app = express();
@@ -16,6 +17,14 @@ app.use(bodyParser.json());
   require('./api/api-routes').default(app);
 
   app.use(require('./api/middlewares/errorHandler').errorHandler);
+
+  if (!config.IS_PODUCTION) {
+    app.use(
+      '/api-docs',
+      swaggerUi.serve,
+      swaggerUi.setup(require('./swagger.json')),
+    );
+  }
 
   app.listen(config.PORT, () => {
     console.log(`Server listen on port: ${config.PORT}`);
