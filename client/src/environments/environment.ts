5b5f243063876b4a6fa3194f55011aa2138ba766
FrizenVladyslav <FrizenVladyslav>
2020-04-11 04:56:37 +0300

    update host

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-const url = 'http://localhost:5000';
+const url = `${window.origin}`;
 
 export const environment = {
   production: false,
